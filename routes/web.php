8380e5a3a3ed7ba8a5ba04d467affefc7badb622
shuji <lteusg7820@yahoo.co.jp>
2022-07-09 13:19:38 +0000

    Blade

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
@@ -25,3 +25,7 @@ Route::group(['prefix' => 'admin'], function() {
      Route::get('profile/create', 'Admin\ProfileController@add');
      Route::get('profile/edit ', 'Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
